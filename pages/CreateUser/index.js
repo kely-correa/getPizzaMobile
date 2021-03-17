55186b5d03810e56b213a1bb5f1bb99725036476
Kely <kelly_le_23mai@hotmail.com>
2021-03-17 20:08:38 -0300

    feat:adding form validation on CreateUser

--- pages/CreateUser/index.js
+++ pages/CreateUser/index.js
@@ -5,13 +5,20 @@ import { Input, Button } from "react-native-elements";
 import styles from './styles';
 import api from '../../services/api';
 import { useNavigation } from '@react-navigation/native';
+import * as Yup from 'yup';
+
+const CreateUserSchema = Yup.object().shape({
+    userName: Yup.string().required('Esse campo é obrigatório'),
+    email: Yup.string().email('Deve ser um email válido').required('Esse campo é obrigatório'),
+    password: Yup.string().min(6, 'A senha deve ter no mínimo 6 caracteres').required('esse campo é obrigatório')
+});
 
 const CreateUser = () => {
 
     const navigation = useNavigation();
 
     const initialValues = {
-        UserName: '',
+        userName: '',
         password: '',
         email: ''
     }
@@ -36,20 +43,23 @@ const CreateUser = () => {
         <Formik
             initialValues={initialValues}
             onSubmit={values => saveUser(values)}
+            validationSchema={CreateUserSchema}
         >
-            {({ handleChange , handleSubmit, handleBlur, values}) => (
+            {({ handleChange , handleSubmit, handleBlur, values, errors, touched, isValid }) => (
                 <View style={styles.container}>
                     <Input 
-                        placeholder='UserName'
+                        placeholder='userName'
                         value={values.name}
-                        onChangeText={handleChange('UserName')}
-                        onBlur={handleBlur('UserName')}
+                        onChangeText={handleChange('userName')}
+                        onBlur={handleBlur('userName')}
+                        errorMessage={errors.userName && touched.userName ? errors.userName : null}
                     />
                     <Input 
                         placeholder='Email'
                         value={values.name}
                         onChangeText={handleChange('email')}
                         onBlur={handleBlur('email')}
+                        errorMessage={errors.email && touched.email ? errors.email : null}
                     />
                     <Input 
                         placeholder='Senha'
@@ -57,11 +67,12 @@ const CreateUser = () => {
                         onChangeText={handleChange('password')}
                         onBlur={handleBlur('password')}
                         secureTextEntry
+                        errorMessage={errors.password && touched.password ? errors.password : null}
                     />
                     <Button 
                         title="Criar conta"
                         onPress={handleSubmit}
-                        disabled={values.name === '' || values.email === '' || values.password === ''}
+                        disabled={!isValid}
                     />
                 </View>
             )}
